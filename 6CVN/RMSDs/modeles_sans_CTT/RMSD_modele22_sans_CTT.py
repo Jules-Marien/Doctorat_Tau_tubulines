ba41669a6478b3025d847beb85a74e76e1b92c4d
Jules-Marien <98336603+Jules-Marien@users.noreply.github.com>
2022-12-22 15:54:11 +0100

    Add files via upload

--- 6CVN/RMSDs/modeles_sans_CTT/RMSD_modele22_sans_CTT.py
+++ 6CVN/RMSDs/modeles_sans_CTT/RMSD_modele22_sans_CTT.py
@@ -12,7 +12,7 @@ modele = '22'
 
 
 
-"""RMSD of the whole system (alignment on the cores)"""
+"""RMSD of the whole system (alignment on the whole system)"""
 
 
 u = mda.Universe( '../PDB_protein_modele'+modele+'_sans_CTT.pdb', '../aligned_traj_reduite_modele'+modele+'_sans_CTT.dcd')
@@ -29,7 +29,7 @@ ref.trajectory[0] #set ref to first frame
 
 
 
-align_selection = "resid 1:426 or resid 445:882 or resid 896:1321"
+align_selection = "protein"
 
 rmsd_selection = "protein"
 
@@ -41,15 +41,15 @@ rmsd_selection = "backbone and " + rmsd_selection
 
 #Align based on align_selection
 
-aligner = align.AlignTraj(u,ref, select=align_selection, in_memory=True)
+aligner = align.AlignTraj(u,ref, select=align_selection, in_memory=True).run()
 
 
 
 #If you don’t have enough memory to do that, write the trajectory out to a file and reload it into MDAnalysis (uncomment the cell below). Otherwise, you don’t have to run it.
 
-# aligner = align.AlignTraj(u, ref, select=align_selection,
-#                           filename='aligned_to_first_frame.dcd').run()
-# u = mda.Universe(PSF, 'aligned_to_first_frame.dcd')
+#aligner = align.AlignTraj(u, ref, select=align_selection,
+ #                          filename='aligned_to_first_frame.dcd').run()
+#u = mda.Universe('../PDB_protein_modele'+modele+'_beta1_avec_tau.pdb', 'aligned_to_first_frame.dcd')
 
 
 
@@ -73,208 +73,6 @@ for ts in u.trajectory:
     
 
 
-
-
-
-
-"""RMSD of core A (alignment on core A)"""
-
-
-u = mda.Universe( '../PDB_protein_modele'+modele+'_sans_CTT.pdb', '../aligned_traj_reduite_modele'+modele+'_sans_CTT.dcd')
-
-ref = mda.Universe( '../PDB_protein_modele'+modele+'_sans_CTT.pdb', '../aligned_traj_reduite_modele'+modele+'_sans_CTT.dcd')
-
-
-
-len_traj = len(u.trajectory)
-
-
-
-ref.trajectory[0] #set ref to first frame 
-
-
-
-align_selection = "resid 1:426"
-
-rmsd_selection = "resid 1:426"
-
-align_selection = "backbone and "  + align_selection
-
-rmsd_selection = "backbone and " + rmsd_selection
-
-
-
-#Align based on align_selection
-
-aligner = align.AlignTraj(u,ref, select=align_selection, in_memory=True)
-
-
-
-#If you don’t have enough memory to do that, write the trajectory out to a file and reload it into MDAnalysis (uncomment the cell below). Otherwise, you don’t have to run it.
-
-# aligner = align.AlignTraj(u, ref, select=align_selection,
-#                           filename='aligned_to_first_frame.dcd').run()
-# u = mda.Universe(PSF, 'aligned_to_first_frame.dcd')
-
-
-
-
-
-
-RMSD_core_A = np.zeros((len_traj))
-
-
-ref_backbone = ref.select_atoms(rmsd_selection)
-
-
-frame = 0
-for ts in u.trajectory:
-    
-    u_backbone = u.select_atoms(rmsd_selection)
-
-    RMSD_core_A[frame] = rms.rmsd(u_backbone.positions, ref_backbone.positions, superposition=False)
-
-    frame = frame +1
-
-
-
-
-
-
-
-
-
-
-"""RMSD of core B (alignment on core B)"""
-
-
-u = mda.Universe( '../PDB_protein_modele'+modele+'_sans_CTT.pdb', '../aligned_traj_reduite_modele'+modele+'_sans_CTT.dcd')
-
-ref = mda.Universe( '../PDB_protein_modele'+modele+'_sans_CTT.pdb', '../aligned_traj_reduite_modele'+modele+'_sans_CTT.dcd')
-
-
-
-len_traj = len(u.trajectory)
-
-
-
-ref.trajectory[0] #set ref to first frame 
-
-
-
-align_selection = "resid 445:882"
-
-rmsd_selection = "resid 445:882"
-
-align_selection = "backbone and "  + align_selection
-
-rmsd_selection = "backbone and " + rmsd_selection
-
-
-
-#Align based on align_selection
-
-aligner = align.AlignTraj(u,ref, select=align_selection, in_memory=True)
-
-
-
-#If you don’t have enough memory to do that, write the trajectory out to a file and reload it into MDAnalysis (uncomment the cell below). Otherwise, you don’t have to run it.
-
-# aligner = align.AlignTraj(u, ref, select=align_selection,
-#                           filename='aligned_to_first_frame.dcd').run()
-# u = mda.Universe(PSF, 'aligned_to_first_frame.dcd')
-
-
-
-
-
-
-RMSD_core_B = np.zeros((len_traj))
-
-
-ref_backbone = ref.select_atoms(rmsd_selection)
-
-
-frame = 0
-for ts in u.trajectory:
-    
-    u_backbone = u.select_atoms(rmsd_selection)
-
-    RMSD_core_B[frame] = rms.rmsd(u_backbone.positions, ref_backbone.positions, superposition=False)
-
-    frame = frame +1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""RMSD of core C (alignment on core C)"""
-
-
-u = mda.Universe( '../PDB_protein_modele'+modele+'_sans_CTT.pdb', '../aligned_traj_reduite_modele'+modele+'_sans_CTT.dcd')
-
-ref = mda.Universe( '../PDB_protein_modele'+modele+'_sans_CTT.pdb', '../aligned_traj_reduite_modele'+modele+'_sans_CTT.dcd')
-
-
-
-len_traj = len(u.trajectory)
-
-
-
-ref.trajectory[0] #set ref to first frame 
-
-
-
-align_selection = "resid 896:1321"
-
-rmsd_selection = "resid 896:1321"
-
-align_selection = "backbone and "  + align_selection
-
-rmsd_selection = "backbone and " + rmsd_selection
-
-
-
-#Align based on align_selection
-
-aligner = align.AlignTraj(u,ref, select=align_selection, in_memory=True)
-
-
-
-#If you don’t have enough memory to do that, write the trajectory out to a file and reload it into MDAnalysis (uncomment the cell below). Otherwise, you don’t have to run it.
-
-# aligner = align.AlignTraj(u, ref, select=align_selection,
-#                           filename='aligned_to_first_frame.dcd').run()
-# u = mda.Universe(PSF, 'aligned_to_first_frame.dcd')
-
-
-
-
-
-
-RMSD_core_C = np.zeros((len_traj))
-
-
-ref_backbone = ref.select_atoms(rmsd_selection)
-
-
-frame = 0
-for ts in u.trajectory:
-    
-    u_backbone = u.select_atoms(rmsd_selection)
-
-    RMSD_core_C[frame] = rms.rmsd(u_backbone.positions, ref_backbone.positions, superposition=False)
-
-    frame = frame +1
 
 
 
@@ -315,7 +113,7 @@ rmsd_selection = "backbone and " + rmsd_selection
 
 #Align based on align_selection
 
-aligner = align.AlignTraj(u,ref, select=align_selection, in_memory=True)
+aligner = align.AlignTraj(u,ref, select=align_selection, in_memory=True).run()
 
 
 
@@ -359,7 +157,7 @@ for ts in u.trajectory:
 
 
 
-"""RMSD of CTT A (alignment on all cores)"""
+"""RMSD of CTT A (alignment on CTT A)"""
 
 """
 u = mda.Universe( '../PDB_protein_modele'+modele+'_sans_CTT.pdb', '../aligned_traj_reduite_modele'+modele+'_sans_CTT.dcd')
@@ -376,7 +174,7 @@ ref.trajectory[0] #set ref to first frame
 
 
 
-align_selection = "resid 1:426 or resid 445:882 or resid 896:1321"
+align_selection = "resid 427:444"
 
 rmsd_selection = "resid 427:444"
 
@@ -388,7 +186,7 @@ rmsd_selection = "backbone and " + rmsd_selection
 
 #Align based on align_selection
 
-aligner = align.AlignTraj(u,ref, select=align_selection, in_memory=True)
+aligner = align.AlignTraj(u,ref, select=align_selection, in_memory=True).run()
 
 
 
@@ -399,8 +197,8 @@ aligner = align.AlignTraj(u,ref, select=align_selection, in_memory=True)
 # u = mda.Universe(PSF, 'aligned_to_first_frame.dcd')
 
 
-"""
 
+"""
 
 
 RMSD_CTT_A = np.ones((len_traj))*(-1)
@@ -416,8 +214,7 @@ RMSD_CTT_A = np.ones((len_traj))*(-1)
 
 
 
-
-"""RMSD of CTT B (alignment on all cores)"""
+"""RMSD of CTT B (alignment on CTT B)"""
 
 """
 u = mda.Universe( '../PDB_protein_modele'+modele+'_sans_CTT.pdb', '../aligned_traj_reduite_modele'+modele+'_sans_CTT.dcd')
@@ -434,7 +231,7 @@ ref.trajectory[0] #set ref to first frame
 
 
 
-align_selection = "resid 1:426 or resid 445:882 or resid 896:1321"
+align_selection = "resid 883:895"
 
 rmsd_selection = "resid 883:895"
 
@@ -446,7 +243,7 @@ rmsd_selection = "backbone and " + rmsd_selection
 
 #Align based on align_selection
 
-aligner = align.AlignTraj(u,ref, select=align_selection, in_memory=True)
+aligner = align.AlignTraj(u,ref, select=align_selection, in_memory=True).run()
 
 
 
@@ -457,8 +254,8 @@ aligner = align.AlignTraj(u,ref, select=align_selection, in_memory=True)
 # u = mda.Universe(PSF, 'aligned_to_first_frame.dcd')
 
 
-
 """
+
 
 
 RMSD_CTT_B = np.ones((len_traj))*(-1)
@@ -474,8 +271,7 @@ RMSD_CTT_B = np.ones((len_traj))*(-1)
 
 
 
-
-"""RMSD of CTT C (alignment on all cores)"""
+"""RMSD of CTT C (alignment on CTT C)"""
 
 """
 u = mda.Universe( '../PDB_protein_modele'+modele+'_sans_CTT.pdb', '../aligned_traj_reduite_modele'+modele+'_sans_CTT.dcd')
@@ -492,7 +288,7 @@ ref.trajectory[0] #set ref to first frame
 
 
 
-align_selection = "resid 1:426 or resid 445:882 or resid 896:1321"
+align_selection = "resid 1322:1339"
 
 rmsd_selection = "resid 1322:1339"
 
@@ -504,7 +300,7 @@ rmsd_selection = "backbone and " + rmsd_selection
 
 #Align based on align_selection
 
-aligner = align.AlignTraj(u,ref, select=align_selection, in_memory=True)
+aligner = align.AlignTraj(u,ref, select=align_selection, in_memory=True).run()
 
 
 
@@ -514,8 +310,8 @@ aligner = align.AlignTraj(u,ref, select=align_selection, in_memory=True)
 #                           filename='aligned_to_first_frame.dcd').run()
 # u = mda.Universe(PSF, 'aligned_to_first_frame.dcd')
 
-
 """
+
 
 
 
@@ -533,7 +329,75 @@ RMSD_CTT_C = np.ones((len_traj))*(-1)
 
 
 
-"""RMSD of R2 (alignment on all cores)"""
+
+
+
+
+
+"""RMSD of all CTTs  (alignment on all CTTs)"""
+
+"""
+u = mda.Universe( '../PDB_protein_modele'+modele+'_sans_CTT.pdb', '../aligned_traj_reduite_modele'+modele+'_sans_CTT.dcd')
+
+ref = mda.Universe( '../PDB_protein_modele'+modele+'_sans_CTT.pdb', '../aligned_traj_reduite_modele'+modele+'_sans_CTT.dcd')
+
+
+
+len_traj = len(u.trajectory)
+
+
+
+ref.trajectory[0] #set ref to first frame 
+
+
+
+align_selection = "resid 427:444 or resid 883:895 or resid 1322:1339"
+
+rmsd_selection = "resid 427:444 or resid 883:895 or resid 1322:1339"
+
+align_selection = "backbone and "  + align_selection
+
+rmsd_selection = "backbone and " + rmsd_selection
+
+
+
+#Align based on align_selection
+
+aligner = align.AlignTraj(u,ref, select=align_selection, in_memory=True).run()
+
+
+
+#If you don’t have enough memory to do that, write the trajectory out to a file and reload it into MDAnalysis (uncomment the cell below). Otherwise, you don’t have to run it.
+
+# aligner = align.AlignTraj(u, ref, select=align_selection,
+#                           filename='aligned_to_first_frame.dcd').run()
+# u = mda.Universe(PSF, 'aligned_to_first_frame.dcd')
+
+
+"""
+
+
+
+RMSD_all_CTTs = np.ones((len_traj))*(-1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""RMSD of R2 (alignment on R2)"""
 
 
 u = mda.Universe( '../PDB_protein_modele'+modele+'_sans_CTT.pdb', '../aligned_traj_reduite_modele'+modele+'_sans_CTT.dcd')
@@ -562,7 +426,7 @@ rmsd_selection = "backbone and " + rmsd_selection
 
 #Align based on align_selection
 
-aligner = align.AlignTraj(u,ref, select=align_selection, in_memory=True)
+aligner = align.AlignTraj(u,ref, select=align_selection, in_memory=True).run()
 
 
 
@@ -608,13 +472,11 @@ for ts in u.trajectory:
 
 RMSD = np.array([np.arange(0,len_traj),
                   RMSD_whole_system,
-                  RMSD_core_A,
-                  RMSD_core_B,
-                  RMSD_core_C,
                   RMSD_all_cores,
                   RMSD_CTT_A,
                   RMSD_CTT_B,
                   RMSD_CTT_C,
+                  RMSD_all_CTTs,
                   RMSD_R2])
 
 
@@ -628,14 +490,12 @@ import pandas as pd
 
 
 column_names = ["Frame", 
-                "RMSD of the whole system (alignment on cores)", 
-                "RMSD of tubulin core A (alignement on tubulin core A)", 
-                "RMSD of tubulin core B (alignement on tubulin core B)",
-                "RMSD of tubulin core C (alignement on tubulin core C)",
+                "RMSD of the whole system (alignment on whole system)", 
                 "RMSD of all cores (alignement on cores)",
-                "RMSD of CTT A (beta chain, alignment on core A)",
-                "RMSD of CTT B (beta chain, alignment on core B)",
-                "RMSD of CTT C (beta chain, alignment on core C)",
+                "RMSD of CTT A (beta chain, alignment on CTT A)",
+                "RMSD of CTT B (beta chain, alignment on CTT B)",
+                "RMSD of CTT C (beta chain, alignment on CTT C)",
+                "RMSD of all CTTs (alignment on all CTTs)",
                 "RMSD of R2 (alignment on R2)"]
 
 df = pd.DataFrame(data= RMSD.T,
